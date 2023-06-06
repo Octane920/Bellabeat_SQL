8009dc88a1913931bc84a8358e3ca8fc1fb2fc4e
Octane920 <129312812+Octane920@users.noreply.github.com>
2023-06-06 11:02:48 -0400

    Update bellabeat.sql

--- bellabeat.sql
+++ bellabeat.sql
@@ -1,4 +1,4 @@
-# Finding the number of unique Id's in daily activity and renaming table as "daily_activity"
+# Finding the number of unique Id's in the daily-activity table and renaming aliase as "daily_activity"
 
 SELECT DISTINCT Id
 
@@ -7,10 +7,14 @@ FROM `bellabeat-capstone-389013.bellabeat_data_v1.daily_activity` AS daily_activ
 
 ; 
 
-# Finding the number of unique Id's in the sleep_log table and renanaming table as sleep_log
+# Finding the number of unique Id's in the sleep_log table and renanaming aliase as sleep_log
 SELECT DISTINCT Id
 FROM bellabeat-capstone-389013.bellabeat_data_v1.sleep_log AS sleep_log
 # determining that there are 24 unique Id's and therefore 24 surveyers that recorded there sleep during the duration of they survey
 
 ;
 
+# Finding the number of unique Id's in the weight_log and renaming the aliase as weight_log
+SELECT DISTINCT Id
+FROM bellabeat-capstone-389013.bellabeat_data_v1.weight_log AS weight_log
+# determined that there are 8 unique Id's and there were only 8 participants who logged their weight during the survey
