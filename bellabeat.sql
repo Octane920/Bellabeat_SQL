16502e834161777ec79473a11bf59cf539cfeaf6
Octane920 <129312812+Octane920@users.noreply.github.com>
2023-06-06 11:46:07 -0400

    Update bellabeat.sql

--- bellabeat.sql
+++ bellabeat.sql
@@ -18,3 +18,24 @@ FROM bellabeat-capstone-389013.bellabeat_data_v1.sleep_log AS sleep_log
 SELECT DISTINCT Id
 FROM bellabeat-capstone-389013.bellabeat_data_v1.weight_log AS weight_log
 # determined that there are 8 unique Id's and there were only 8 participants who logged their weight during the survey
+
+;
+
+# determining if there were any duplicate entries in the daily_activity table
+SELECT ID, Activity_Date, COUNT(*) AS dups
+FROM `bellabeat_data_v1.daily_activity`
+GROUP BY ID, Activity_Date 
+HAVING dups > 1
+# determined that there were 0 duplicates
+
+;
+
+# Determining if there were any duplicate entries within the sleep_log table
+SELECT ID, SleepDay , COUNT(*) AS dups
+FROM `bellabeat_data_v1.sleep_log`
+GROUP BY ID, SleepDay 
+HAVING dups > 1
+# Identified 3 duplicates
+
+;
+
