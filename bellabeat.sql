2103bca8b1ff4fc4c378f3ff5c5d05a95743d50b
Octane920 <129312812+Octane920@users.noreply.github.com>
2023-06-06 10:54:20 -0400

    Update bellabeat.sql

--- bellabeat.sql
+++ bellabeat.sql
@@ -3,7 +3,14 @@
 SELECT DISTINCT Id
 
 FROM `bellabeat-capstone-389013.bellabeat_data_v1.daily_activity` AS daily_activity
-#determined that there are 33 distince Id's and therefore 33 surverers in this study
+# determined that there are 33 distince Id's and therefore 33 surverers in this study
 
 ; 
+
+# Finding the number of unique Id's in the sleep_log table and renanaming table as sleep_log
+SELECT DISTINCT Id
+FROM bellabeat-capstone-389013.bellabeat_data_v1.sleep_log AS sleep_log
+# determining that there are 24 unique Id's and therefore 24 surveyers that recorded there sleep during the duration of they survey
+
+;
 
