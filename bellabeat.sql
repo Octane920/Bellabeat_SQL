d89e5106e101731cafd7fba0d9ce5d1b71754b80
Octane920 <129312812+Octane920@users.noreply.github.com>
2023-06-06 10:49:27 -0400

    Update bellabeat.sql

--- bellabeat.sql
+++ bellabeat.sql
@@ -1,5 +1,9 @@
-# Finding the number of unique ID's in daily activity
+# Finding the number of unique Id's in daily activity and renaming table as "daily_activity"
 
 SELECT DISTINCT Id
 
  FROM `bellabeat-capstone-389013.bellabeat_data_v1.daily_activity` AS daily_activity
+#determined that there are 33 distince Id's and therefore 33 surverers in this study
+
+; 
+
